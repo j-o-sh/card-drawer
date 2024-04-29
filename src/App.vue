36566adc25c5013a0655cc8631f994b5a75c73cb
Josh <59149060+j-o-sh@users.noreply.github.com>
2024-04-29 09:58:43 +0200

    basic shuffle and card drawing mechanic

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,33 @@
 import { computed, onMounted, reactive, ref } from 'vue';
 
 const names = ['John', 'Jim', 'James', 'Josh', 'Jesse', 'Jackob']
+
+const x = reactive({
+  deck: [...names],
+  skew: [],
+  drawn: [],
+  status: 'done',
+
+  reshuffle() {
+    this.drawn = []
+    this.deck = names.toSorted(() => Math.random() - .5)
+    this.skew = [...this.deck].map(() => Math.random() * 10 - 5)
+    this.draw()
+  },
+
+  draw() {
+    const next = this.deck.pop()
+    if (next) {
+      this.drawn.push(next)
+    } else {
+      this.drawn = []
+    }
+  },
+
+  skewAt(index) {
+    return this.skew[index] || 0
+  }
+})
 </script>
 
 <template>
@@ -11,8 +38,14 @@ const names = ['John', 'Jim', 'James', 'Josh', 'Jesse', 'Jackob']
     Simple ✌️ 
   </p>
   <main class="card-deck">
-    <Card is="section" @click="drawCard" backside>
-    </Card>
+    <Card 
+      v-if="x.drawn.length"
+      v-for="card, i in x.drawn"
+      is="section" 
+      @click="() => x.draw()" 
+      :style="`--skew-angle: ${x.skewAt(i)}deg;`"
+    >{{ card }}</Card>
+    <Card v-else backside @click="() => x.reshuffle()"/>
     <!-- <Menu class="vertical-menu"/> -->
   </main>
 </template>
@@ -24,11 +57,17 @@ h1, p {
 }
 .card-deck {
   display: flex;
+  position: relative;
   justify-content: center;
   gap: .5em;
   padding: 2em;
-  & * {
+
+  & > * {
     font-size: 2.5em;
+    position: absolute;
+    border: 2px solid var(--ctp-base);
+
+    transform: rotate(var(--skew-angle));
   }
 }
 .vertical-menu {
