7534485909ee4cc843dde7ce5b6d52a8ed470e5b
Josh <59149060+j-o-sh@users.noreply.github.com>
2024-05-11 12:28:19 +0200

    better form style

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,13 +1,11 @@
 <script setup>
+
 /** @type {{value: Array[string]}} */
 const model = defineModel()
 const emit = defineEmits('submit')
 
 function onchange({ target }) {
-  if (target.name === 'new') {
-    model.value.push(target.value)
-    target.value = ""
-  } else if (!target.value || target.value.trim() === '') {
+  if (!target.value || target.value.trim() === '') {
     //TODO there is something weired here with the focs ordering... It skips, 
     //     since we delete the input.
     model.value.splice(Number(target.name), 1)
@@ -16,17 +14,40 @@ function onchange({ target }) {
   }
 }
 
+function addOnChange({ target }) {
+  model.value.push(target.value)
+  target.value = ""
+  setTimeout(() => target.scrollIntoView({
+    block: 'start',
+    behaviour: 'smooth'
+  }))
+}
+
 function reset() {
   model.value = []
+}
+
+function intoView(e) {
+  e.target.scrollIntoView({
+    block: 'center',
+    behaviour: 'smooth'
+  })
 }
 </script>
 
 <template>
-  <Card><form class="scribble" @submit.prevent="" @reset="reset">
+  <Card class="form-card">
+    <form class="scribble" @submit.prevent="" @reset="reset">
     <label v-for="name, i in model">
-      <input :name="i" :value="name" @change="onchange">
+      <input :name="i" :value="name" @change="onchange" @focus="intoView">
     </label>
-    <label><input name="new" value="" @change="onchange"></label>
+    <label><input 
+        name="new" 
+        value=""
+        placeholder="Add a name here"
+        @change="addOnChange"
+        @focus="intoView"
+      ></label>
 
     <!-- HTML you quirky sometimes 🤪 -->
     <input type="submit" style="display:none" disabled>
@@ -41,7 +62,8 @@ function reset() {
         <input type="reset" :disabled="!model?.length">
       </label>
     </div>
-  </form></Card>
+  </form>
+  </Card>
 </template>
 
 <style scoped>
@@ -57,10 +79,21 @@ input {
 
 label:has(input[type=submit], input[type=reset]) {
   font-size: 1rem;
+  transition: all 125ms;
+
   & input {
     position: fixed;
     margin-top: -200vh;
   }
+
+  &:not(:has([disabled])):hover {
+    transform: scale(1.15);
+    text-shadow: 0 0 2px var(--ctp-overlay0);
+  }
+}
+
+label:has(input[type=submit]) {
+  font-size: 1.25rem;
 }
 
 label:has(input[disabled]) {
@@ -72,10 +105,29 @@ label:has(.fa):focus-within {
   text-shadow: 0 0 3px var(--ctp-crust);
 }
 
+form {
+  max-height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: relative;
+}
+
+.form-card {
+  padding: 1rem;
+  --card-color: var(--ctp-lavender);
+  background-color: var(--card-color);
+}
+
 .controls {
   display: flex;
   gap: .5rem;
   justify-content: center;
+  align-items: center;
   margin-top: 1rem;
+  background: var(--card-color);
+
+  position: sticky;
+  bottom: 0;
 }
+
 </style>
